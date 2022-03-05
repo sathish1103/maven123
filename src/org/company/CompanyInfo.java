2dd322c76a8d4d8a9ad21212991811e4fee4bbc0
sathish1103 <csathish1103@gmail.com>
2022-03-05 18:48:42 +0530

    REP123

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,30 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+		System.out.println("greensTech");
+	}
+	
+	private void companyId() {
+		System.out.println("greens@gmail.com");
+	}
+	private void companyAddress() {
+		System.out.println("perungudi");
+	}
+	
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
